29187289fab95563aed81a98331ea96f14c4b74e
minnjeongpark <thoroughlynew@gmail.com>
2022-10-09 19:49:25 +0900

    구현/조합-17142(연구소3) - dfs로 풀었으나 시간초과. 조합으로 푸니까 통과함 - dfs 사용 시 조합이 중복되지 않도록 처리 -> start 관건

--- ss/17142-연구소3.py
+++ ss/17142-연구소3.py
@@ -60,47 +60,46 @@ def spread_virus(atv_virus):
                 if board[nx][ny] == 0:
                     q.append((nx, ny))
                     v[nx][ny] = v[x][y] + 1
-                    answer = max(answer, v[nx][ny]) # 이 과정에서 초(최대값) 찾기
+                    answer = max(answer, v[nx][ny]) # ** 이 과정에서 초(최대값) 찾기
                 elif board[nx][ny] == 2:  # 비활성 바이러스 칸
                     q.append((nx, ny))
                     v[nx][ny] = v[x][y] + 1
-
     virus_area = 0
     for i in range(N):
         for j in range(N):
             if v[i][j] != -1:
                 virus_area += 1
     if max_virus_area != virus_area:
-        return -1
+        return float('inf')
     return answer
 
 virus_v = [[False]*N for _ in range(N)]
 
 
 # 무슨 바이러스를 깨울건지 선택
-def select_virus(start, virus_count, tmp_virus_list):
-    global min_time, activate_virus
-    if virus_count == M:
-        # 바이러스 퍼뜨리기
-        tmp_time = spread_virus(tmp_virus_list)
-        if tmp_time == -1:
-            return
-        if 0 <= tmp_time < min_time:
-            activate_virus = tmp_virus_list
-            min_time = tmp_time
-        return
-    else:
-        # for i in range(start, M):
-        #     vp = virus_pos[i]
-        #     if not virus_v[vp[0]][vp[1]]:
-        #         virus_v[vp[0]][vp[1]] = True
-        #         select_virus(start + 1, virus_count + 1, tmp_virus_list + [vp])
-        #         virus_v[vp[0]][vp[1]] = False
-        for vp in virus_pos[start:]:
-            if not virus_v[vp[0]][vp[1]]:
-                virus_v[vp[0]][vp[1]] = True
-                select_virus(start+1, virus_count+1, tmp_virus_list + [vp])
-                virus_v[vp[0]][vp[1]] = False
+# def select_virus(start, virus_count, tmp_virus_list):
+#     global min_time, activate_virus
+#     if virus_count == M:
+#         # 바이러스 퍼뜨리기
+#         tmp_time = spread_virus(tmp_virus_list)
+#         if tmp_time == -1:
+#             return
+#         if 0 <= tmp_time < min_time:
+#             activate_virus = tmp_virus_list
+#             min_time = tmp_time
+#         return
+#     else:
+#         # for i in range(start, M):
+#         #     vp = virus_pos[i]
+#         #     if not virus_v[vp[0]][vp[1]]:
+#         #         virus_v[vp[0]][vp[1]] = True
+#         #         select_virus(start + 1, virus_count + 1, tmp_virus_list + [vp])
+#         #         virus_v[vp[0]][vp[1]] = False
+#         for vp in virus_pos[start:]:
+#             if not virus_v[vp[0]][vp[1]]:
+#                 virus_v[vp[0]][vp[1]] = True
+#                 select_virus(start+1, virus_count+1, tmp_virus_list + [vp])
+#                 virus_v[vp[0]][vp[1]] = False
 
 # select_virus(0, 0, [])
 #
@@ -114,4 +113,4 @@ else:
     print(min_time)
     # print(max_virus_area)
 
-print("time :", time.time() - start)
+# print("time :", time.time() - start)
